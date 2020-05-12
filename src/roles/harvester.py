2fd085abd38b6bb22878c0522718c5e2f02bae8f
Michael Mugnai <michael.mugnai@gmail.com>
2020-05-12 22:28:26 +0200

    my harvester

--- src/roles/harvester.py
+++ src/roles/harvester.py
@@ -10,47 +10,61 @@ __pragma__('noalias', 'type')
 __pragma__('noalias', 'update')
 
 
-def run_harvester(creep):
+def run_harvester(creep: Creep):
     """
     Runs a creep as a generic harvester.
-    :param creep: The creep to run
+    It will collect energy from its closest source, then put it on any structure or upgrade the controller if they're full.
     """
 
+    " decide whether to go to collect sources or to deposit them "
     # If we're full, stop filling up and remove the saved source
-    if creep.memory.filling and _.sum(creep.carry) >= creep.carryCapacity:
+    if creep.memory.filling and creep.store.getFreeCapacity() == 0:
         creep.memory.filling = False
         del creep.memory.source
     # If we're empty, start filling again and remove the saved target
-    elif not creep.memory.filling and creep.carry.energy <= 0:
+    elif not creep.memory.filling and creep.store.getUsedCapacity() == 0:
         creep.memory.filling = True
         del creep.memory.target
 
+    " source collection logic "
     if creep.memory.filling:
         # If we have a saved source, use it
         if creep.memory.source:
-            source = Game.getObjectById(creep.memory.source)
+            source = Game.getObjectById(creep.memory.source)  # TODO: store position directly?
         else:
-            # Get a random new source and save it
-            source = _.sample(creep.room.find(FIND_SOURCES))
+            # first look for dropped sources
+            source = creep.pos.findClosestByPath(FIND_DROPPED_RESOURCES)
+            if source is not None:
+                creep.memory.recover_dropped_source = True
+            else:
+                # Get the closest source and store it as the chosen one
+                source = creep.pos.findClosestByPath(FIND_SOURCES, {"filter": lambda s: s.energy > 0})
             creep.memory.source = source.id
 
         # If we're near the source, harvest it - otherwise, move to it.
-        if creep.pos.isNearTo(source):
+        if creep.pos.isNearTo(source):  # TODO: it's better to check isNearTo or try to harvest and check ERR_NOT_IN_RANGE?
+            if creep.memory.recover_dropped_source:
+                creep.pickup(source)
+                del creep.memory.recover_dropped_source
             result = creep.harvest(source)
             if result != OK:
-                print("[{}] Unknown result from creep.harvest({}): {}".format(creep.name, source, result))
+                print(f"[{creep.name}] Unknown result from creep.harvest({source}): {result}")
         else:
             creep.moveTo(source)
     else:
+        " target deposit logic "
         # If we have a saved target, use it
         if creep.memory.target:
             target = Game.getObjectById(creep.memory.target)
         else:
-            # Get a random new target.
-            target = _(creep.room.find(FIND_STRUCTURES)) \
-                .filter(lambda s: ((s.structureType == STRUCTURE_SPAWN or s.structureType == STRUCTURE_EXTENSION)
-                                   and s.energy < s.energyCapacity) or s.structureType == STRUCTURE_CONTROLLER) \
-                .sample()
+            # Get the new target as the closest one
+            target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+                "filter": lambda s: (s.structureType == STRUCTURE_SPAWN or
+                                     s.structureType == STRUCTURE_EXTENSION) and s.energy < s.energyCapacity})
+            # if all structures are full, go to upgrade the controller
+            if target is None:
+                target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+                    "filter": lambda s: s.structureType == STRUCTURE_CONTROLLER})
             creep.memory.target = target.id
 
         # If we are targeting a spawn or extension, we need to be directly next to it - otherwise, we can be 3 away.
@@ -60,7 +74,7 @@ def run_harvester(creep):
             is_close = creep.pos.inRangeTo(target, 3)
 
         if is_close:
-            # If we are targeting a spawn or extension, transfer energy. Otherwise, use upgradeController on it.
+            # If we are targeting a spawn or extension, transfer energy. Otherwise, use upgradeController on it
             if target.energyCapacity:
                 result = creep.transfer(target, RESOURCE_ENERGY)
                 if result == OK or result == ERR_FULL:
